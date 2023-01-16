19a9cad6de1da1fb81bc919567d71d6da029db57
Jiri Cerny <cerny@arit.cz>
2023-01-16 17:15:17 +0100

    PostPresenter

---
+++ app/Presenters/PostPresenter.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\UI\Form;
+
+final class PostPresenter extends Nette\Application\UI\Presenter
+{
+	private Nette\Database\Explorer $database;
+
+	public function __construct(Nette\Database\Explorer $database)
+	{
+		$this->database = $database;
+	}
+
+	public function renderShow(int $postId): void
+    {
+        $post = $this->database
+            ->table('posts')
+            ->get($postId);
+        if (!$post) {
+            $this->error('Stránka nebyla nalezena');
+        }
+
+        $this->template->post = $post;
+    }
+
+}
+?>
